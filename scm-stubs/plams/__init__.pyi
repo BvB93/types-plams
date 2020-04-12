337f9f2e6d2d885516b2e355d51efe1d57cdc8bb
Bas van Beek <b.f.van.beek@vu.nl>
2020-04-12 21:34:42 +0200

    WiP

--- scm-stubs/plams/__init__.pyi
+++ scm-stubs/plams/__init__.pyi
@@ -1,6 +1,6 @@
 from .mol.molecule import Molecule
-from .mol.atoms import Atoms
-from .mol.bonds import Bonds
+from .mol.atoms import Atom
+from .mol.bonds import Bond
 
-__all__: List[str]
+__all__: List[str] = ['Molecule', 'Atom', 'Bond']
 __version__: str
